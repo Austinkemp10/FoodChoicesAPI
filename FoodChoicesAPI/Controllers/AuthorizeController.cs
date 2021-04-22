10274b8d9497e5f440dd510ed62287b13edfdfd4
Austin Kempker <austin@kempker.com>
2021-04-22 11:18:25 -0700

    Coordinate Added

---
+++ FoodChoicesAPI/Controllers/AuthorizeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodChoicesAPI.Controllers
+{
+    [ApiController]
+    [Route("authorize")]
+    public class AuthorizeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("~/Views/Authorize/Index.cshtml");
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
--- FoodChoicesAPI/Data/FoodChoicesContext.cs
+++ FoodChoicesAPI/Data/FoodChoicesContext.cs
@@ -12,6 +12,11 @@ namespace FoodChoicesAPI.Data
     {
         public DbSet<Users> Users { get; set; }
 
+        public FoodChoicesContext(DbContextOptions<FoodChoicesContext> options) : base(options)
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL("server=localhost;database=foodchoices;user=root;password=root");
@@ -30,6 +35,25 @@ namespace FoodChoicesAPI.Data
                 entity.Property(e => e.Age);
                 entity.Property(e => e.DateCreated);
                 entity.Property(e => e.Deleted);
+            });
+
+            modelBuilder.Entity<Restaurants>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Category).IsRequired();
+                entity.Property(e => e.City);
+                entity.Property(e => e.State);
+                entity.Property(e => e.County);
+                entity.Property(e => e.ImageName);
+            });
+
+            modelBuilder.Entity<Coordinate>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.RestaurantId).IsRequired();
+                entity.Property(e => e.Latitude).IsRequired();
+                entity.Property(e => e.Longitude).IsRequired();
             });
 
         }
